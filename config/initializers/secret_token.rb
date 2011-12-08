74feaacd6b07a28a28447fa42d6823110c527965
Stefan Goodchild <me@stefangoodchild.com>
2011-12-08 15:37:33 +0000

    Initial Rails 3.2 install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eventstreams2::Application.config.secret_token = 'f7b27093bf7e70a7ac308682a1f2630ccc67e0dbc4e714baff8ee582ca0c3167f6a9798c703cebd2cff83ffcaf68859580a45001c7cfa1ae2f5cd44778f6234b'
