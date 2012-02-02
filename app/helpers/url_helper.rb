8a19735d7b685cbff259a1c901679cbfc6d46bc9
Stefan Goodchild <me@stefangoodchild.com>
2012-02-02 15:27:21 +0000

    Horrid hack but might just sort my subdomains for staging.

--- app/helpers/url_helper.rb
+++ app/helpers/url_helper.rb
@@ -1,16 +1,17 @@
 module UrlHelper  
-
   def with_subdomain(subdomain)  
     subdomain = (subdomain || "")  
-    subdomain += "." unless subdomain.empty?  
-    [subdomain, request.domain].join  
+    subdomain += "." unless subdomain.empty?
+    stage = request.subdomain(SUBDOMAIN_LEVEL-1)+'.' if request.subdomain(SUBDOMAIN_LEVEL-1).present?
+    
+    [subdomain, stage, request.domain].join  
   end
   
-  # def url_for(options = nil)  
-  #   if options.kind_of?(Hash) && options.has_key?(:subdomain)  
-  #     options[:host] = with_subdomain(options.delete(:subdomain))  
-  #   end  
-  #   super  
-  # end  
+  def url_for(options = nil)  
+    if options.kind_of?(Hash) && options.has_key?(:subdomain)  
+      options[:host] = with_subdomain(options.delete(:subdomain))  
+    end  
+    super  
+  end  
 
 end
